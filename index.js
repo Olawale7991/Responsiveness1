78b472cad25545b28e0ef9fb6a91d97cbcd515d0
Olawale7991 <olawalelokoso000@gmail.com>
2024-05-18 02:17:17 +0100

    responsiveness

---
+++ index.js
@@ -0,0 +1,23 @@
+let chinedu = 1000
+let money = 5000
+let total = money + chinedu 
+
+console.log(total)
+
+let rasak=true;
+let vicyor="hello word";
+let complete=2000;
+let empty=null;
+
+console.log(typeof rasak);
+console.log(typeof vicyor);
+console.log(typeof complete);
+console.log(typeof empty);
+
+let upper = "Hello";
+let visitor =10;
+
+if(visitor<=10){
+    alert("welcome to our website");
+}
+else {alert ("Thank you for visiting our website")}
